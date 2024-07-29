52312d0b62572fe0fcd4e622a2dd9ce34d8361cb
Luca Cortinovis <luca1cortinovis@gmail.com>
2024-07-29 18:03:22 +0200

    added courses operations

--- SelectQueryResult.cs
+++ SelectQueryResult.cs
@@ -207,7 +207,7 @@ public class SelectQueryResult {
 
     }
 
-    public static void print_student(DataTable table_result) {
+    static void print_student(DataTable table_result) {
 
         Table table = new Table();
         table.Config = TableConfiguration.Unicode();
@@ -223,7 +223,21 @@ public class SelectQueryResult {
         Console.Write(table.ToString());
     }
 
-    
+    static void print_student_short(DataTable table_result) {
+
+        Table table = new Table();
+        table.Config = TableConfiguration.Unicode();
+
+        for (int i = 0; i < table_result.Columns.Count; ++i) {
+            table.AddColumn(table_result.Columns[i].ColumnName);
+        }
+
+        for (int i = 0; i < table_result.Rows.Count; ++i) {
+            table.AddRow(table_result.Rows[i][0].ToString(), table_result.Rows[i][1].ToString(), table_result.Rows[i][2].ToString(),
+                table_result.Rows[i][3].ToString());
+        }
+        Console.Write(table.ToString());
+    }
 
 
     public static void menu_show_professor(string str_connection) {
@@ -245,11 +259,12 @@ public class SelectQueryResult {
             Console.WriteLine("2. Search professors by course ");
             Console.WriteLine("3. Search professors by name ");
             Console.WriteLine("4. Search professors by surname ");
-            Console.WriteLine("5. Search professors by year of teaching ");
+            Console.WriteLine("5. Search professors by email ");
+            Console.WriteLine("6. Search professors by year of teaching ");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("6. Clear screen ");
+            Console.WriteLine("7. Clear screen ");
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("7. Close (or return to previous menu) ");
+            Console.WriteLine("8. Close (or return to previous menu) ");
             Console.ResetColor();
             Console.WriteLine();
 
@@ -260,7 +275,8 @@ public class SelectQueryResult {
 
                 case 1:
 
-                    query = "SELECT professor_id AS 'ProfessorID', first_name AS 'Name', last_name AS 'Surname' " +
+                    query = "SELECT professor_id AS 'ProfessorID', " +
+                            "first_name AS 'Name', last_name AS 'Surname', email AS 'Email'" +
                             "FROM professor;";
 
                     table_result = execute_select(str_connection, query);
@@ -284,7 +300,8 @@ public class SelectQueryResult {
                     }
                     Console.WriteLine();
 
-                    query = @"SELECT professor.professor_id AS 'ProfessorID', first_name AS 'Name', last_name AS 'Surname' " +
+                    query = @"SELECT professor.professor_id AS 'ProfessorID', " +
+                             "first_name AS 'Name', last_name AS 'Surname', email AS 'Email' " +
                              "FROM professor " +
                              "JOIN course_professor " +
                                  "ON professor.professor_id = course_professor.professor_id " +
@@ -316,9 +333,10 @@ public class SelectQueryResult {
                     }
                     Console.WriteLine();
 
-                    query = @"SELECT professor_id AS 'ProfessorID', first_name AS 'Name', last_name AS 'Surname' " +
-                            "FROM professor " +
-                            "WHERE first_name = @str_search;";
+                    query = @"SELECT professor_id AS 'ProfessorID', " +
+                             "first_name AS 'Name', last_name AS 'Surname', email AS 'Email' " +
+                             "FROM professor " +
+                             "WHERE first_name = @str_search;";
 
                     table_result = execute_select(str_connection, query, nameof(str_search), str_search);
 
@@ -341,9 +359,10 @@ public class SelectQueryResult {
                     }
                     Console.WriteLine();
 
-                    query = @"SELECT professor_id AS 'ProfessorID', first_name AS 'Name', last_name AS 'Surname' " +
-                            "FROM professor " +
-                            "WHERE last_name = @str_search;";
+                    query = @"SELECT professor_id AS 'ProfessorID', " +
+                             "first_name AS 'Name', last_name AS 'Surname', email AS 'Email' " +
+                             "FROM professor " +
+                             "WHERE last_name = @str_search;";
 
                     table_result = execute_select(str_connection, query, nameof(str_search), str_search);
 
@@ -358,6 +377,32 @@ public class SelectQueryResult {
                     break;
 
                 case 5:
+                    Console.WriteLine("Insert the email: ");
+                    str_search = UniMain.check_input_string();
+                    if (string.IsNullOrWhiteSpace(str_search)) {
+                        UniMain.log_error_message("You must write an email! \n");
+                        break;
+                    }
+                    Console.WriteLine();
+
+                    query = @"SELECT professor_id AS 'ProfessorID', " +
+                             "first_name AS 'Name', last_name AS 'Surname', email AS 'Email' " +
+                             "FROM professor " +
+                             "WHERE email = @str_search;";
+
+                    table_result = execute_select(str_connection, query, nameof(str_search), str_search);
+
+                    if (table_result.Rows.Count == 0) {
+                        UniMain.log_error_message($"No professor found with email {str_search}! \n");
+                        break;
+                    }
+
+                    print_professor(table_result);
+
+                    Console.WriteLine();
+                    break;
+
+                case 6:
                     Console.WriteLine("Insert the academic year: ");
                     num_search = UniMain.check_input_int();
                     if (num_search <= 0 || num_search >= 4) {
@@ -366,15 +411,17 @@ public class SelectQueryResult {
                     }
                     Console.WriteLine();
 
-                    query = @"SELECT professor.professor_id AS 'ProfessorID', professor.first_name AS 'Name', professor.last_name AS 'Surname' " +
-                            "FROM professor " +
-                            "JOIN course_professor " +
+                    query = @"SELECT professor.professor_id AS 'ProfessorID', " +
+                             "professor.first_name AS 'Name', professor.last_name AS 'Surname', " +
+                             "professor.email AS 'Email'" +
+                             "FROM professor " +
+                             "JOIN course_professor " +
                                 "ON professor.professor_id = course_professor.professor_id " +
-                            "JOIN course " +
+                             "JOIN course " +
                                 "ON course.course_id = course_professor.course_id " +
-                            "WHERE course.academic_year = @num_search " +
-                            "GROUP BY professor.professor_id " +
-                            "ORDER BY professor.professor_id;";
+                             "WHERE course.academic_year = @num_search " +
+                             "GROUP BY professor.professor_id " +
+                             "ORDER BY professor.professor_id;";
 
                     table_result = execute_select(str_connection, query, nameof(num_search), num_search);
 
@@ -388,15 +435,15 @@ public class SelectQueryResult {
                     Console.WriteLine();
                     break;
 
-                case 6:
+                case 7:
                     Console.Clear();
                     break;
 
-                case 7:
+                case 8:
                     break;
             }
 
-        } while (choice != 7);
+        } while (choice != 8);
 
     }
 
@@ -410,7 +457,8 @@ public class SelectQueryResult {
         }
 
         for (int i = 0; i < table_result.Rows.Count; ++i) {
-            table.AddRow(table_result.Rows[i][0].ToString(), table_result.Rows[i][1].ToString(), table_result.Rows[i][2].ToString());
+            table.AddRow(table_result.Rows[i][0].ToString(), table_result.Rows[i][1].ToString(), 
+                table_result.Rows[i][2].ToString(), table_result.Rows[i][3].ToString());
         }
         Console.Write(table.ToString());
     }
@@ -434,10 +482,12 @@ public class SelectQueryResult {
             Console.WriteLine("1. Show all courses ");
             Console.WriteLine("2. Search courses by title ");
             Console.WriteLine("3. Search courses by academic year ");
+            Console.WriteLine("4. Search all courses by student ");
+            Console.WriteLine("5. Search all students by course ");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("4. Clear screen ");
+            Console.WriteLine("6. Clear screen ");
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("5. Close (or return to previous menu) ");
+            Console.WriteLine("7. Close (or return to previous menu) ");
             Console.ResetColor();
             Console.WriteLine();
 
@@ -514,14 +564,70 @@ public class SelectQueryResult {
                     break;
 
                 case 4:
-                    Console.Clear();
+                    Console.WriteLine("Insert the student ID: ");
+                    num_search = UniMain.check_input_int();
+                    if (num_search <= 0) {
+                        UniMain.log_error_message("Invalid student ID! \n");
+                        break;
+                    }
+                    Console.WriteLine();
+
+                    query = @"SELECT course.course_id AS 'CourseID', course.title AS 'Title', course.academic_year AS 'Academic year' " +
+                            "FROM course " +
+                            "JOIN course_student ON course.course_id = course_student.course_id " +
+                            "JOIN student ON student.student_id = course_student.student_id " +
+                            "WHERE student.student_id = @num_search;";
+
+                    table_result = execute_select(str_connection, query, nameof(num_search), num_search);
+
+                    if (table_result.Rows.Count == 0) {
+                        UniMain.log_error_message($"No course found by student {num_search}! \n");
+                        break;
+                    }
+
+                    print_course(table_result);
+
+                    Console.WriteLine();
                     break;
 
                 case 5:
+                    Console.WriteLine("Insert the course ID: ");
+                    num_search = UniMain.check_input_int();
+                    if (num_search <= 0) {
+                        UniMain.log_error_message("Invalid course ID! \n");
+                        break;
+                    }
+                    Console.WriteLine();
+
+                    query = @"SELECT student.student_id AS 'StudentID', " + 
+                             "student.first_name AS 'Name', student.last_name AS 'Surname', " +
+                             "student.email AS 'Email' " +
+                            "FROM student " +
+                            "JOIN course_student ON student.student_id = course_student.student_id " +
+                            "JOIN course ON course.course_id = course_student.course_id " +
+                            "WHERE course.course_id = @num_search;";
+
+                    table_result = execute_select(str_connection, query, nameof(num_search), num_search);
+
+                    if (table_result.Rows.Count == 0) {
+                        UniMain.log_error_message($"No student found by course {num_search}! \n");
+                        break;
+                    }
+
+                    print_student_short(table_result);
+
+                    Console.WriteLine();
+                    break;
+
+                case 6:
+                    Console.Clear();
+                    break;
+
+                case 7:
                     break;
             }
 
-        } while (choice != 5);
+        } while (choice != 7);
 
     }
 
